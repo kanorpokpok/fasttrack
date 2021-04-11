4b51ce456d454a5d2c262129dad2a1c8d55e1e50
kanorpokpok <kanorpokpok@gmail.com>
2021-04-11 20:48:41 +0700

    cx
    d

---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { MenuModule } from 'primeng/menu';
+import { ChipModule } from 'primeng/chip';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { TableModule } from 'primeng/table';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ConfirmationService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MessageService } from 'primeng/api';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdduserdailogComponent } from './components/dailog/user/adduserdailog/adduserdailog.component';
+import { DialogService } from 'primeng/dynamicdialog';
+import { LoginComponent } from './components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AdduserdailogComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    MenuModule,
+    ChipModule,
+    ToolbarModule,
+    MegaMenuModule,
+    TableModule,
+    PanelMenuModule,
+    ConfirmPopupModule,
+    ConfirmDialogModule,
+    ToastModule
+  ],
+  entryComponents: [AdduserdailogComponent],
+  providers: [MessageService, DialogService, ConfirmationService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
